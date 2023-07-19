4ef901ef1d7c1c882d3931c5e072d693f9e7d949
pachangeprachi <85255535+pachangeprachi@users.noreply.github.com>
2023-07-19 10:16:57 +0530

    constants

--- lib/resources/string_manager.dart
+++ lib/resources/string_manager.dart
@@ -7,18 +7,20 @@ class AppString {
   static const String signin = "Sign in";
 
   ///Forgot password screen
-  static const String  forgotpasshead= "Forgot \nPassword ?";
+  static const String forgotpasshead = "Forgot \nPassword ?";
   static const String forgotsubtitle1 = "Please enter your email and tap ";
   static const String forgotsubtitle2 = "Request to Change";
   static const String changepassbtn = "Request to change Password";
+  static const String notFoundRoute = "No Rout Found";
 
   ///Thank you screen
   static const String thanku = "Thank you ";
-  static const String thankusubtitle1 = "You request for password change is\nsuccessfully processed.";
-  static const String thankusubtitle2 = "Please Contact your admin for a new\npassword";
+  static const String thankusubtitle1 =
+      "You request for password change is\nsuccessfully processed.";
+  static const String thankusubtitle2 =
+      "Please Co ntact your admin for a new\npassword";
   static const String okay = "Okay";
 
   ///Home Screen
   static const String home = "Home";
-
 }
